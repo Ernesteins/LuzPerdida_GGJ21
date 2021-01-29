74cc0fed61322e5ff91de45c812ee02e0994dca2
IsmaLB <bravoismael26@gmail.com>
2021-01-29 18:40:25 -0500

    Modificacion al enemigo y capsulas de deteccion

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider))]
+public class EnemyAttack : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player"){
+            Debug.Log("Game Over!!");
+            //To Test reload the scene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}

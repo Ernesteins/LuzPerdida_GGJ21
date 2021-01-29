9185a6504c25ed53047b128b567222aac049bf77
IsmaLB <bravoismael26@gmail.com>
2021-01-28 19:53:13 -0500

    Agregando Enemy movement

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour
 {
+    [SerializeField] Transform chaseTarget = null;
+    NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        agent.SetDestination(chaseTarget.position);
+    }
+    void Chase(){
         
+    }
+    void Patrol(){
+
     }
 }

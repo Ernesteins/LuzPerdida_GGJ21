1e63225878bef517bcfa942669413587ece2a558
IsmaLB <bravoismael26@gmail.com>
2021-01-30 02:03:27 -0500

    Adiign Ui elements and scripts

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public delegate void PauseEvent(bool isPaused);
+    public delegate void GameEvent();
+    public static event PauseEvent OnPause;
+    public static event GameEvent OnGameOver;
+    public static event GameEvent OnWin;
+    public static GameManager instance;
+    private void Awake() {
+        instance = this;
+    }
+
+    bool paused = false;
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            Pause();
+        }        
+    }
+    public void Pause(){
+        paused = !paused;
+        OnPause?.Invoke(paused);
+        Time.timeScale = paused? 0 : 1;
+    }
+    public void GameOver(){
+        OnGameOver?.Invoke();
+        Time.timeScale = 0;
+    }
+    public void Win(){
+        OnWin?.Invoke();
+    }
+}
---
+++ Assets/Scripts/UI/OnGameCanvasController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnGameCanvasController : MonoBehaviour
+{
+    [SerializeField] GameObject pauseScreen = null;   
+    [SerializeField] GameObject gameOverScreen = null;   
+    [SerializeField] GameObject winScreen = null;   
+    private void OnEnable() {
+        GameManager.OnPause += ActivatePauseMenu;
+        GameManager.OnGameOver += ActivateGameOverMenu;
+    }
+    private void OnDisable() {
+        GameManager.OnPause -= ActivatePauseMenu;
+        GameManager.OnGameOver -= ActivateGameOverMenu;
+    }
+    void ActivateWinMenu(){
+        gameOverScreen.SetActive(true);
+    }
+    void ActivateGameOverMenu(){
+        gameOverScreen.SetActive(true);
+    }
+    void ActivatePauseMenu(bool isPaused){
+        pauseScreen.SetActive(isPaused);
+    }
+    public void Pause(){
+        GameManager.instance.Pause();
+    }
+}
